e936941ca275154ea8a0ee93f9da3bfcfbba2555
Morgan <morganrconnolly@berkeley.edu>
2016-07-20 15:56:55 -0700

    added models through SessionActions

--- server/models/models.py
+++ server/models/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 #association table for User/Student many-to-many relationship
 students = db.Table('students',
@@ -6,21 +7,43 @@ students = db.Table('students',
     db.Column('student_id', db.Integer, db.ForeignKey('student.id'))
 )
 
-
-class Student(db.Model):
+class Group(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), index=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-	studentQ = db.Column(db.String(64), index=True)
-	deleted = db.Column(db.Boolean, default=False)
-	parents = db.relationship('User',
-							secondary=students,
-							secondaryjoin=(students.c.student_id==id),
-							lazy='dynamic')
+	name = db.Column(db.String(64))
+	isActive = db.Column(db.Boolean, default=True)
+
+	#many-to-one relationship with Mentor
+	mentor_id = db.Column(db.Integer, 
+							db.ForeignKey('mentor.id'))
+
+	#one-to-many relationship with Student
+	students = db.relationship('Student',
+								backref='group',
+								lazy='dynamic')
+
+	#one-to-many relationship with Session
+	sessions = db.relationship('Session',
+								backref='group',
+								lazy='dynamic')
 
 
-	def __repr__(self):
-		return '<Student %r>' % (self.name)
+	def add_student(self,student):
+		if not self.has_student(student):
+			self.students.append(student)
+			return self
+
+	def has_student(self,student):
+		return self.students.filter(student.id==student.id).count() > 0
+
+	def has_session(self, session):
+		return self.sessions.filter(session.id==session.id).count() > 0
+
+	def add_session(self, session):
+		if not self.has_session(session):
+			self.sessions.append(session)
+			return self
+
+
 
 
 
@@ -31,11 +54,29 @@ class User(db.Model):
 	username = db.Column(db.String(64))
 	email = db.Column(db.String(120))
 	admin = db.Column(db.Boolean, default=False)
+	deleted = db.Column(db.Boolean, default=False)
+
+	#many-to-many relationship with students
 	students = db.relationship('Student', 
 								secondary=students,
 								primaryjoin=(students.c.parent_id==id),
 								lazy='dynamic')
-	deleted = db.Column(db.Boolean, default=False)
+
+	#one-to-many relationship with SessionAction
+	sessionActions = db.relationship('SessionAction',
+										backref='user',
+										lazy='dynamic')
+
+	def add_student(self, student):
+		if not self.has_student(student):
+			self.students.append(student)
+			return self
+
+	def has_student(self, student):
+		return self.students.filter(students.c.student_id==student.id).count() > 0
+
+
+	
 
 	#the below properties are expected by the flask-login module
 	@property 
@@ -49,13 +90,7 @@ class User(db.Model):
 	def is_anonymous(self):
 		return False
 
-	def add_student(self, student):
-		if not self.has_student(student):
-			self.students.append(student)
-			return self
 
-	def has_student(self, student):
-		return self.students.filter(students.c.student_id==student.id).count() > 0
 
 
 	def get_id(self):
@@ -69,3 +104,92 @@ class User(db.Model):
 
 	def __repr__(self):
 		return '<User %r>' % (self.nickname)
+
+
+class Student(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), index=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	studentQ = db.Column(db.String(64), index=True)
+	deleted = db.Column(db.Boolean, default=False)
+	#many-to-many relationship with Users
+	parents = db.relationship('User',
+							secondary=students,
+							secondaryjoin=(students.c.student_id==id),
+							lazy='dynamic')
+	#many-to-one relationship with Group
+	group_id = db.Column(db.Integer, 
+							db.ForeignKey('group.id'))
+
+	def has_parent(self, user):
+		return self.parents.filter(students.c.parent_id==user.id).count() > 0
+
+	def __repr__(self):
+		return '<Student %r>' % (self.name)
+
+class StudentRates(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	rate = db.Column(db.Integer)
+	description = db.Column(db.String(120))
+
+class Mentor(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(120))
+	isActive = db.Column(db.Boolean, default=True)
+	email = db.Column(db.String(120))
+	phone = db.Column(db.String(20))
+
+	#one-to-many relationship with Group
+	groups = db.relationship('Group',
+								backref='mentor',
+								lazy='dynamic')
+
+	def has_group(self,group):
+		return self.groups.filter(group.id==group.id).count() > 0
+
+	def add_group(self,group):
+		if not self.has_group(group):
+			self.groups.append(group)
+			return self
+
+class Session(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+	DateTime = db.Column(db.DateTime)
+	isCanceled = db.Column(db.Boolean, default=False)
+
+	#one-to-many relationship with SessionAction
+	SessionActions = db.relationship('SessionAction',
+										backref='session',
+										lazy = 'dynamic')
+
+	def add_SessionAction(self, SessionAction):
+		if not has_SessionAction(SessionAction):
+			self.SessionActions.append(SessionAction)
+			return self
+
+	def has_SessionAction(self, SessionAction):
+		return self.sessionActions.filter(SessionAction.id == SessionAction.id).count > 0
+
+class SessionAction(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+
+	#many-to-one relationship with Session
+	session_id = db.Column(db.Integer, 
+							db.ForeignKey('session.id'))
+
+	#many-to-one relationship with User
+	user_id = db.Column(db.Integer,
+						db.ForeignKey('user.id'))
+	DateTime = db.Column(db.DateTime, default = datetime.utcnow)
+	Type = db.Column(db.String(24))
+	MoveDetails= db.Column(db.String(120))
+
+
+
+
+
+
+
+
+
--- server/tests.py
+++ server/tests.py
@@ -3,7 +3,7 @@ import os
 import unittest
 
 from app import create_test_app
-from models import User, Student
+from models import User, Student, Group, Mentor, Session
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = create_test_app()
@@ -26,7 +26,7 @@ class TestCase(unittest.TestCase):
         db.session.add(u)
         db.session.commit()
 
-    def test_many_to_many(self):
+    def test_parents_to_students(self):
         u1 = User(username='john', email='john@example.com')
         u2 = User(username='kylo',email='k@starkillerbase.com')
         db.session.add(u1)
@@ -44,6 +44,86 @@ class TestCase(unittest.TestCase):
 
         assert u1.has_student(s1)
         assert u1.has_student(s2)
+        assert s1.has_parent(u1)
+
+        u2.add_student(s1)
+        assert s1.has_parent(u2)
+
+    def test_mentor_to_group(self):
+        m = Mentor(name='Snoke')
+
+        g1 = Group(name='Solo')
+        g2 = Group(name='Generals')
+
+        s1 = Student(name='Ray')
+        s2 = Student(name='Kylo')
+        s3 = Student(name='Hux')
+
+
+        db.session.add(m)
+        db.session.add(g1)
+        db.session.add(g2)
+        db.session.add(s1)
+        db.session.add(s2)
+        db.session.add(s3)
+        db.session.commit()
+
+        g1.add_student(s1)
+        g1.add_student(s2)
+        g2.add_student(s3)
+
+        m.add_group(g1)
+        m.add_group(g2)
+        
+        assert g1.mentor.name == 'Snoke'
+        assert m.has_group(g2)
+
+    def test_session_to_group(self):
+        m = Mentor(name='Snoke')
+
+        g1 = Group(name='Solo')
+        g2 = Group(name='Generals')
+
+        s1 = Student(name='Ray')
+        s2 = Student(name='Kylo')
+        s3 = Student(name='Hux')
+
+
+        db.session.add(m)
+        db.session.add(g1)
+        db.session.add(g2)
+        db.session.add(s1)
+        db.session.add(s2)
+        db.session.add(s3)
+        db.session.commit()
+
+        sesh1 = Session()
+        g1.add_session(sesh1)
+        db.session.add(sesh1)
+        db.session.commit()
+
+        g1.add_student(s1)
+        g1.add_student(s2)
+        g2.add_student(s3)
+
+        assert g1.has_session(sesh1)
+        assert sesh1.group_id == g1.id
+
+    def test_SessionActions(self):
+        u1 = User('Han')
+        u2 = User('Leia')
+
+        sesh1 = Session()
+        sesh2 = Session()
+
+        seshA1 = SessionAction(type='moved')
+        seshA2 = SessionAction(type='confirmed')
+
+        
+
+
+    
+
 
 
 
